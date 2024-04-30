9dff44b80400409205fff7621f327acf24bc222a
Daniel Román <danielroman.05111@gmail.com>
2024-04-30 00:14:06 -0500

    feat(TeacherResolver) Handling GraqhQl errors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EntityNotFoundErrorFilter } from './entitiy-not-found-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new EntityNotFoundErrorFilter());
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Application running on ${await app.getUrl()}`);
