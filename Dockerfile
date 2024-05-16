099e6d6cbe3363a12530d8fa500e7b5b033fe80b
Daniel Roman2+git config --global user.name Daniel <danielroman.05111@gmail.com>
2024-05-16 06:43:23 +0000

    fix: Dockerfile store-dir

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /app
 FROM base AS prod-deps
 RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --frozen-lockfile
 
-RUN pnpm config set store-dir /pnpm-store
+RUN pnpm config set store-dir ~/pnpm-store
 RUN apk add git
 
 EXPOSE 3000 
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new EntityNotFoundErrorFilter());
+
+  
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Application running on ${await app.getUrl()}`);
