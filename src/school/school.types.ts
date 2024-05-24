c89f1bdbda674a83db2217267826cf5d4bb51790
Daniel Roman2+git config --global user.name Daniel <danielroman.05111@gmail.com>
2024-05-24 04:34:35 +0000

    feat: Entities with id string or number class definition on ObjectTypes

--- src/school/school.types.ts
+++ src/school/school.types.ts
@@ -1,9 +1,29 @@
-import { registerEnumType } from '@nestjs/graphql';
+import { Field, Int, ObjectType, registerEnumType } from '@nestjs/graphql';
 
 export enum Gender {
   Male = 'Male',
   Female = 'Female',
   Other = 'Other',
+}
+
+@ObjectType()
+export class EntityWithNumberId {
+  constructor(id: number) {
+    this.id = id;
+  }
+
+  @Field(() => Int)
+  id: number;
+}
+
+@ObjectType()
+export class EntityWithStringId {
+  constructor(id: string) {
+    this.id = id;
+  }
+
+  @Field(() => String)
+  id: string;
 }
 
 registerEnumType(Gender, { name: 'Gender' });
