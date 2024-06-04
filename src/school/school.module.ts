262f0b9ae13729774b8dc62ed6b6f8862dd243f6
Daniel Roman2+git config --global user.name Daniel <danielroman.05111@gmail.com>
2024-06-04 03:17:56 +0000

    feat: SubjectResolver and CourseResolver

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subject } from './subject.entity';
 import { Teacher } from './teacher.entity';
-import { TrainingController } from './training.controller';
 import { TeacherResolver } from './teacher.resolver';
-import { Course } from './course.entity';
+import { SubjectResolver } from './subject.resolver';
+import { CourseResolver } from './course.resolver';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Subject, Teacher, Course])],
-  controllers: [TrainingController],
-  providers: [TeacherResolver],
+  imports: [TypeOrmModule.forFeature([Subject, Teacher])],
+  providers: [TeacherResolver, SubjectResolver, CourseResolver],
+  controllers: [],
 })
 export class SchoolModule {}
