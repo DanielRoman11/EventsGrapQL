72693ffa10ea6ea5766084b86c170670af610967
Daniel Roman2+git config --global user.name Daniel <danielroman.05111@gmail.com>
2024-06-07 03:26:50 +0000

    updated packages and logging

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EntityNotFoundErrorFilter } from './entitiy-not-found-error.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
+  const app = await NestFactory.create(AppModule, {
+    // logger: ['verbose', 'debug', 'error', 'warn'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new EntityNotFoundErrorFilter());
+  app.useLogger(new Logger('AppContext', { timestamp: true }));
 
-  
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
-  console.log(`Application running on ${await app.getUrl()}`);
+  logger.verbose(`Application running on ${await app.getUrl()}`);
 }
 bootstrap();
